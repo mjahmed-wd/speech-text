9eff9f5df2104db92caf69b8751ed0714440983f
Jubair <jubair@metadesignsolutions.co.uk>
2023-12-14 14:49:37 +0600

    fast api dockerization added

---
+++ README.md
@@ -0,0 +1,7 @@
+https://github.com/SYSTRAN/faster-whisper
+
+
+<!-- Sample Request body -->
+```
+{"fileUrl": "https://raw.githubusercontent.com/mjahmed-wd/speech-text/main/files/audio.mp3"}
+```
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+
+# @app.post("/transcribe/")
+# async def transcribe(request: Request):
+#     fileUrl = request.client.fileUrl
+#     result = transcribe_file(fileUrl)
+#     return {"data": result}
