b66145741c4ce81a0ba7c5dded8682a9c23b64df
Jubair <jubair@metadesignsolutions.co.uk>
2023-12-15 17:40:48 +0600

    average sample size sent on response

--- model.py
+++ model.py
@@ -7,23 +7,36 @@ model = WhisperModel(model_size_or_path, device="auto", compute_type='int8', cpu
 def transcribe_file(fileUrl):
     print('Started transcribing file: ' + fileUrl)
     
-    segments, _ = model.transcribe(fileUrl, beam_size=5, word_timestamps=True)
 
     segment_list = []
+
+
+    # For 5-10 second chunk
+    segments, _ = model.transcribe(fileUrl, beam_size=1)
     for segment in segments:
-        word_list = []
-        for word in segment.words:
-            word_dict = {
-                "confidence": word.probability,
-                "startTime": word.start,
-                "endTime": word.end,
-                "word": word.word
-            }
-            word_list.append(word_dict)
         segment_dict = {
-            "words": word_list
+            "startTime": segment.start,
+            "endTime": segment.end,
+            "text": segment.text
         }
         segment_list.append(segment_dict)
+
+    # For word level timestamps
+    # segments, _ = model.transcribe(fileUrl, beam_size=5, word_timestamps=True)
+    # for segment in segments:
+    #     word_list = []
+    #     for word in segment.words:
+    #         word_dict = {
+    #             "confidence": word.probability,
+    #             "startTime": word.start,
+    #             "endTime": word.end,
+    #             "word": word.word
+    #         }
+    #         word_list.append(word_dict)
+    #     segment_dict = {
+    #         "words": word_list
+    #     }
+    #     segment_list.append(segment_dict)
 
     print('Finished transcribing file: ' + fileUrl)
 
